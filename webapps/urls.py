11ac490f4c24eb7740d5dc843640df8c536cfd24
Jainam Manoj Gala <jgala@andrew.cmu.edu>
2024-08-21 21:39:25 -0400

    added swagger docs

--- webapps/urls.py
+++ webapps/urls.py
@@ -18,6 +18,24 @@ from django.contrib import admin
 from django.urls import path
 from cmumarketplace import views
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tartan Mart APIs",
+        default_version='v1',
+        description="APIs for Tartan Mart",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="webdev@andrew.cmu.edu"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     # path('setcsrfcookie/', views.set_csrf_cookie, name='set-csrf-cookie'),
     path('token/', views.validate_google_oauth_token, name='validate-token'),
@@ -31,4 +49,6 @@ urlpatterns = [
     path('getOrderDetails/<int:id>/', views.get_order_details, name='get-order-details'),
     path('getOrder/<int:id>/', views.get_order, name= "get_order"),
     path('updateItemStatus/<int:id>/', views.update_item_status, name="update_item"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
