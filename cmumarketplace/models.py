d0826e88904050d513864c2aac53ccd538bc2637
Jainam Manoj Gala <jgala@andrew.cmu.edu>
2024-08-27 03:32:41 -0400

    listing model and sold item model

--- cmumarketplace/models.py
+++ cmumarketplace/models.py
@@ -54,8 +54,9 @@ class UserProfile(models.Model):
         address (str, optional): The user's address.
         mobile (str, optional): The user's mobile number.
         email_contact (str, optional): The user's alternative email contact.
+
     """
-    user = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name= 'user_profile')
+    user = models.ForeignKey(to=CustomUser, on_delete = models.CASCADE, related_name= 'user_profile')
     profile_picture = models.URLField(blank = True, null = True)
     address = models.CharField(max_length=ADDRESS_LIMIT, blank = True, null = True,)
     mobile = models.CharField(max_length=10, blank= True, null = True)
@@ -63,6 +64,46 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f'User (email = {self.user.email})'
+    
+class Listing(models.Model):
+    """
+    Model representing a listing in the system.
+
+    This model stores information about an item listed by a user for sale on Tartan Mart.
+
+    Attributes:
+        user (CustomUser): The user who listed the item.
+        name (str): The name of the item.
+        description (str): A detailed description of the item.
+        price (Decimal): The price of the item.
+        listed_date (DateTime): The date and time when the item was listed.
+        sold_date (DateTime, optional): The date and time when the item was sold.
+        quantity (int): The quantity of the item available for sale.
+        delivery_or_pickup (str): The delivery method for the item (delivery or pickup).
+        current_status (str): The current status of the item listing.
+        category (str): The category of the item.
+        delivery_time (int, optional): The estimated delivery time for the item.
+        pickup_address (str, optional): The pickup address for the item (if pickup option is selected).
+        image (ImageField, optional): An image of the item.
+        image_url (URLField, optional): A URL to an image of the item.
+
+    """
+    user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE, related_name= 'user_listing')
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(null=True, blank=True)
+    CURRENT_STATUS_CHOICES = (
+        ('listed', 'Listed'),
+        ('delisted', 'Delisted'),
+        ('deleted', 'Deleted'),
+    )
+    current_status = models.CharField(max_length=20, choices=CURRENT_STATUS_CHOICES, default='listed')
+    delivery_time = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(30)])
+    pickup_address = models.CharField(max_length=ADDRESS_LIMIT, null=True, blank=True)
+    
+    def __str__(self):
+        return f'Listing - {self.name}, Created by - {self.user}'
 
 class Item(models.Model):
     """
@@ -87,7 +128,7 @@ class Item(models.Model):
         image_url (URLField, optional): A URL to an image of the item.
 
     """
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name= 'item_listing')
+    listing = models.ForeignKey(to=Listing, on_delete=models.CASCADE, related_name='listing_item')
     name = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(blank=False, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
@@ -97,27 +138,78 @@ class Item(models.Model):
     delivery_or_pickup = models.CharField(max_length=20, choices=[('delivery', 'Delivery'), ('pickup', 'Pickup')], default='pickup')
     CURRENT_STATUS_CHOICES = (
         ('listed', 'Listed'),
-        ('sold', 'Sold'),
+        ('delisted', 'Delisted'),
         ('deleted', 'Deleted'),
     )
     current_status = models.CharField(max_length=20, choices=CURRENT_STATUS_CHOICES, default='listed')
     CATEGORY_CHOICES = (
         ('electronics', 'Electronics'),
-        # ('textbooks', 'Textbooks'),
         ('furniture', 'Furniture'),
         ('clothing', 'Clothing'),
         ('home', 'Home'),
         ('beauty', 'Beauty'),
-        # ('toys', 'Toys'),
         ('other', 'Other'),
+        ('none', 'None')
     )
-    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='clothing')
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='none')
     delivery_time = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(30)])
     pickup_address = models.CharField(max_length=ADDRESS_LIMIT, null=True, blank=True)
     image = models.ImageField(upload_to="images/", blank=True, null=True, default=DEFAULT_ITEM_IMAGE)
     image_url = models.URLField(null=True, blank=True)
     def __str__(self):
         return f'Item - {self.name}, Qty - {self.quantity}'
+    
+class SoldItem(models.Model):
+    """
+    Model representing a sold item in the system.
+
+    This model stores information about an item sold by a user on Tartan Mart.
+
+    Attributes:
+        user (CustomUser): The user who sold the item.
+        name (str): The name of the item.
+        description (str): A detailed description of the item.
+        price (Decimal): The price of the item.
+        listed_date (DateTime): The date and time when the item was listed.
+        sold_date (DateTime, optional): The date and time when the item was sold.
+        quantity (int): The quantity of the item sold.
+        delivery_or_pickup (str): The delivery method for the item (delivery or pickup).
+        current_status (str): The current status of the item sold.
+        category (str): The category of the item.
+        delivery_time (int, optional): The estimated delivery time for the item.
+        pickup_address (str, optional): The pickup address for the item (if pickup option is selected).
+        image (ImageField, optional): An image of the item.
+        image_url (URLField, optional): A URL to an image of the item.
+
+    """
+    listing = models.ForeignKey(to=Listing, on_delete=models.CASCADE, related_name='listing_item')
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    listed_date = models.DateTimeField(default=timezone.now)
+    sold_date = models.DateTimeField(null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    delivery_or_pickup = models.CharField(max_length=20, choices=[('delivery', 'Delivery'), ('pickup', 'Pickup')], default='pickup')
+    CURRENT_STATUS_CHOICES = (
+        ('sold', 'Sold'),
+    )
+    current_status = models.CharField(max_length=20, choices=CURRENT_STATUS_CHOICES, default='sold')
+    CATEGORY_CHOICES = (
+        ('electronics', 'Electronics'),
+        ('furniture', 'Furniture'),
+        ('clothing', 'Clothing'),
+        ('home', 'Home'),
+        ('beauty', 'Beauty'),
+        ('other', 'Other'),
+        ('none', 'None')
+    )
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='none')
+    delivery_time = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(30)])
+    pickup_address = models.CharField(max_length=ADDRESS_LIMIT, null=True, blank=True)
+    image = models.ImageField(upload_to="images/", blank=True, null=True, default=DEFAULT_ITEM_IMAGE)
+    image_url = models.URLField(null=True, blank=True)
+    def __str__(self):
+        return f'Sold Item - {self.name}, Qty - {self.quantity}'
     
 '''
 removing order features for live version
@@ -154,7 +246,6 @@ removing order features for live version
 #         quantity (int, optional): The quantity of the item ordered.
 
 #     """
-
 #     order = models.ForeignKey(Order, on_delete=models.CASCADE)
 #     item = models.ForeignKey(Item, on_delete=models.PROTECT)
 #     quantity = models.PositiveIntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(100)])
