e8779c680a9167ddf127e516dfdef7dad65279de
Jainam Manoj Gala <jgala@andrew.cmu.edu>
2024-09-04 22:31:51 -0400

    wip models and serializers

--- cmumarketplace/models.py
+++ cmumarketplace/models.py
@@ -93,6 +93,7 @@ class Listing(models.Model):
     description = models.TextField(blank=False, null=False)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(null=True, blank=True)
+    delivery_or_pickup = models.CharField(max_length=20, choices=[('delivery', 'Delivery'), ('pickup', 'Pickup')], default='pickup')
     CURRENT_STATUS_CHOICES = (
         ('listed', 'Listed'),
         ('delisted', 'Delisted'),
@@ -155,113 +156,7 @@ class Item(models.Model):
     delivery_time = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(30)])
     pickup_address = models.CharField(max_length=ADDRESS_LIMIT, null=True, blank=True)
     image = models.ImageField(upload_to="images/", blank=True, null=True, default=DEFAULT_ITEM_IMAGE)
+    imageb64 = models.CharField(max_length=9999, null=True, blank=True)
     image_url = models.URLField(null=True, blank=True)
     def __str__(self):
         return f'Item - {self.name}, Qty - {self.quantity}'
-    
-class SoldItem(models.Model):
-    """
-    Model representing a sold item in the system.
-
-    This model stores information about an item sold by a user on Tartan Mart.
-
-    Attributes:
-        user (CustomUser): The user who sold the item.
-        name (str): The name of the item.
-        description (str): A detailed description of the item.
-        price (Decimal): The price of the item.
-        listed_date (DateTime): The date and time when the item was listed.
-        sold_date (DateTime, optional): The date and time when the item was sold.
-        quantity (int): The quantity of the item sold.
-        delivery_or_pickup (str): The delivery method for the item (delivery or pickup).
-        current_status (str): The current status of the item sold.
-        category (str): The category of the item.
-        delivery_time (int, optional): The estimated delivery time for the item.
-        pickup_address (str, optional): The pickup address for the item (if pickup option is selected).
-        image (ImageField, optional): An image of the item.
-        image_url (URLField, optional): A URL to an image of the item.
-
-    """
-    listing = models.ForeignKey(to=Listing, on_delete=models.CASCADE, related_name='listing_item')
-    name = models.CharField(max_length=255, blank=False, null=False)
-    description = models.TextField(blank=False, null=False)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
-    listed_date = models.DateTimeField(default=timezone.now)
-    sold_date = models.DateTimeField(null=True, blank=True)
-    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(100)])
-    delivery_or_pickup = models.CharField(max_length=20, choices=[('delivery', 'Delivery'), ('pickup', 'Pickup')], default='pickup')
-    CURRENT_STATUS_CHOICES = (
-        ('sold', 'Sold'),
-    )
-    current_status = models.CharField(max_length=20, choices=CURRENT_STATUS_CHOICES, default='sold')
-    CATEGORY_CHOICES = (
-        ('electronics', 'Electronics'),
-        ('furniture', 'Furniture'),
-        ('clothing', 'Clothing'),
-        ('home', 'Home'),
-        ('beauty', 'Beauty'),
-        ('other', 'Other'),
-        ('none', 'None')
-    )
-    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='none')
-    delivery_time = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(30)])
-    pickup_address = models.CharField(max_length=ADDRESS_LIMIT, null=True, blank=True)
-    image = models.ImageField(upload_to="images/", blank=True, null=True, default=DEFAULT_ITEM_IMAGE)
-    image_url = models.URLField(null=True, blank=True)
-    def __str__(self):
-        return f'Sold Item - {self.name}, Qty - {self.quantity}'
-    
-'''
-removing order features for live version
-'''
-# class Order(models.Model):
-#     """
-#     Model representing an order placed by a user.
-#     Attributes:
-#         buyer (CustomUser): The user who placed the order.
-#         items (ManyToManyField): The items included in the order, linked through the OrderItem model.
-#         total_price (Decimal, optional): The total price of the order.
-#         payment_status (str): The payment status of the order.
-#         order_date (DateTime): The date and time when the order was placed.
-#         checkout_id (str): The ID associated with the checkout process for the order.
-#         delivery_address (str): The delivery address for the order.
-#     """
-#     buyer = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
-#     items = models.ManyToManyField(Item, through='OrderItem')
-#     total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-#     payment_status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed'), ('failed', 'Failed')])
-#     order_date = models.DateTimeField(auto_now_add=True)
-#     checkout_id = models.CharField(max_length=50000)
-#     delivery_address = models.CharField(max_length= ADDRESS_LIMIT)
-
-#     def __str__(self):
-#         return f"Order {self.id} - Buyer: {self.buyer.name}, Total Price: {self.total_price}, Payment Status: {self.payment_status}, Checkout ID: {self.checkout_id}"
-    
-# class OrderItem(models.Model):
-#     """
-#     Model representing an item included in an order.
-#     Attributes:
-#         order (Order): The order to which this item belongs.
-#         item (Item): The item included in the order.
-#         quantity (int, optional): The quantity of the item ordered.
-
-#     """
-#     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-#     item = models.ForeignKey(Item, on_delete=models.PROTECT)
-#     quantity = models.PositiveIntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(100)])
-
-#     def increase_item_quantity(self):
-#         """
-#         Increases the quantity of the associated item
-#             and sets its current status to 'listed'.
-#         """
-#         self.item.quantity += self.quantity
-#         self.item.current_status = 'listed'
-#         self.item.save()
-
-#     def __str__(self):
-#         """
-#          Returns a string representation of the OrderItem instance,
-#             including the order ID, item name, and quantity.
-#         """
-#         return f"Order {self.order.id} - Item: {self.item.name}, Quantity: {self.quantity}"
--- cmumarketplace/serializers.py
+++ cmumarketplace/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Item, UserProfile
+from .models import Item, Listing, UserProfile
 
 class TokenSerializer(serializers.Serializer):
     """ JWT Token Serializer """
@@ -13,88 +13,22 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ['id', 'owner', 'address', 'mobile', 'email_contact', 'profile_picture']
 
 class ItemSerializer(serializers.ModelSerializer):
-    """ Item Serializer """
-    owner = serializers.ReadOnlyField(source='user.email')
-    image = serializers.ImageField(required=False, use_url=True)
     class Meta:
         model = Item
-        fields = ['id', 'owner', 'name', 'description', 'price', 'quantity', 'listed_date', 'current_status', 'category', 'delivery_or_pickup', 'pickup_address', 'delivery_time', 'image']
+        fields = ['id', 'name', 'description', 'price', 'quantity', 'category', 'image']
 
+class ListingSerializer(serializers.ModelSerializer):
+    listing_item = ItemSerializer(many=True)
 
-'''
-removing order features for live version
-'''
-# class OrderSerializer(serializers.ModelSerializer):
-#     """ Order Serializer: This serializer converts Order model instances into JSON representation
-#     and vice versa. """
-#     owner = serializers.ReadOnlyField(source='user.email')
-#     items = ItemSerializer(many=True, read_only=True)
-#     class Meta:
-#         model = Order
-#         fields = ['id', 'owner', 'items', 'total_price', 'payment_status', 'order_date', 'checkout_id', 'delivery_address']
+    class Meta:
+        model = Listing
+        fields = ['id', 'name', 'description', 'delivery_or_pickup', 'delivery_time', 'pickup_address', 'listing_item']
 
-# class OrderItemSerializer(serializers.ModelSerializer):
-#     """
-#     Serializer for the OrderItem model.
+    def create(self, validated_data):
+        items_data = validated_data.pop('listing_item')
+        listing = Listing.objects.create(**validated_data)
+        for item_data in items_data:
+            Item.objects.create(listing=listing, **item_data)
+        return listing
 
-#     This serializer converts OrderItem model instances into JSON representation
-#     and vice versa.
-
-#     """
-#     order = OrderSerializer()
-#     item = ItemSerializer()
-#     class Meta:
-#         model = OrderItem
-#         fields = ['id', 'item', 'order', 'quantity']
-
-
-# class ODItemSerializer(serializers.ModelSerializer):
-#     """
-#     Serializer for the Order Detail Item page data.
-
-#     This serializer converts Order Detail Item page data instances into JSON representation
-#     and vice versa.
-
-#     """
-#     class Meta:
-#         model = Item
-#         fields = ['image']
-
-# class ODOrderItemSerializer(serializers.ModelSerializer):
-#     item_name = serializers.ReadOnlyField(source='item.name')
-#     quantity = serializers.IntegerField()
-#     item_image = serializers.SerializerMethodField()
-    
-#     class Meta:
-#         model = OrderItem
-#         fields = ['item_name', 'quantity', 'item_image']
-    
-#     def get_item_image(self, obj):
-#         return obj.item.image.url if obj.item.image else None
-
-# class ODOrderSerializer(serializers.ModelSerializer):
-#     items = ODOrderItemSerializer(source='orderitem_set', many=True, read_only=True)
-    
-#     class Meta:
-#         model = Order
-#         fields = ['id', 'total_price', 'payment_status', 'order_date', 'delivery_address', 'items']
-
-    
-
-
-'''
-never used
-'''
-# class PurchasedItemSerializer(serializers.ModelSerializer):
-#     item = ItemSerializer
-#     class Meta:
-#         model = PurchasedItem
-#         fields = ['id', 'item', 'quantity', 'delivery_or_pickup', 'delivery_address']
-
-# class OrderSerializer(serializers.ModelSerializer):
-#     owner = serializers.ReadOnlyField(source='user.email')
-#     items = PurchasedItemSerializer(many=True, read_only=True)
-#     class Meta:
-#         model = Order
-#         fields = ['id', 'owner', 'items', 'order_date', 'payment_status', 'delivery_or_pickup', 'delivery_address']
 
